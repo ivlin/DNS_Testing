f08fd23b919ed6d34dcffb3a963734bf1c963bb1
ivlin <ivlin112@gmail.com>
2019-07-18 19:57:07 -0400

    tried pickle, not working that well

--- test.py
+++ test.py
@@ -11,14 +11,14 @@ def test_domain(domain, out_path, num_trials=1):
     # for code on running sudo commands in python
     with open(outname, "w") as tcpout:
         #Begin TCPDump
-        sudo_password=fe2d2cd42390e2b300b548eb80f3c23f36ea41
+        #sudo_password=fe2d2cd42390e2b300b548eb80f3c23f36ea41
         command = 'sudo tcpdump'.split()
         p = subprocess.Popen(['sudo', '-S'] + command, stdin=subprocess.PIPE, stderr=tcpout, stdout=tcpout,
           universal_newlines=True)
 
         #automation issue: tcpdump must be run as sudo separately, but we must also pass the password to it
         #for now, it will be done manually
-        sudo_prompt = p.communicate(sudo_password + '\n')[1]
+        #sudo_prompt = p.communicate(sudo_password + '\n')[1]
 
         #Run Browsertime
         print("Testing domain %s"%domain)
@@ -82,6 +82,68 @@ def extract_combinations(filename, group_size):
 
 DATA_DIRECTORY="tcpout"
 
+import cPickle as pickle
+
+def calculate_all_nvm(num_resolvers=2):
+    with open( "%s/1_occurrence.ptmp"%DATA_DIRECTORY,"wb") as firstfile:
+        pickle.dump(set(),firstfile)
+    cumulative_count=["%s/1_occurrence.ptmp"%DATA_DIRECTORY]
+    curproc=psutil.Process()
+    try:
+        ind=0
+        while True:
+            path="%s/%d/dns_main"%(DATA_DIRECTORY, ind)
+            combinations = extract_combinations(path, num_resolvers)
+            for combination in combinations:
+                combination = tuple(sorted(combination))
+                tuple_hash = hash(combination)
+                for freq in xrange(len(cumulative_count)):
+                    with open(cumulative_count[freq],"rb") as datafile_r:
+                        occurence_hashes = pickle.load(datafile_r)
+                    if tuple_hash in occurence_hashes:
+                        occurence_hashes.remove(tuple_hash)
+                        with open(cumulative_count[freq],"wb") as datafile_w:
+                            pickle.dump(occurence_hashes, datafile_w)
+                        if freq+1>=len(cumulative_count):
+                            newname="%s/%d_occurrence.ptmp"%(DATA_DIRECTORY,freq+1)
+                            cumulative_count.append(newname)
+                            nextfile=open(newname,"wb")
+                            nextset=set()
+                        else:
+                            with open(cumulative_count[freq+1],"rb") as nextfile:
+                                nextset=pickle.load(nextfile)
+                            nextfile=open(cumulative_count[freq+1],"wb")
+                        nextset.add(tuple_hash)
+                        pickle.dump(nextset,nextfile)
+                        nextfile.close()
+                        break
+                else:
+                    with open(cumulative_count[0],"rb") as datafile:
+                        data=pickle.load(datafile)
+                    data.add(tuple_hash)
+                    with open(cumulative_count[0],"wb") as datafile:
+                        pickle.dump(data,datafile)
+            print("PROGRESS %d"%ind)
+            ind+=1
+            print("CPU PERCENT: %f"%curproc.cpu_percent())
+            print(psutil.virtual_memory())
+
+    except OSError:
+        with open("%s/cumulative_%dr_summary"%(DATA_DIRECTORY, num_resolvers),"w") as histogram_summary:
+            histogram_summary.write("occurences_of_a_group, frequency\n")
+            with open("%s/cumulative_%dr_data"%(DATA_DIRECTORY, num_resolvers),"w") as histogram_file:
+                for freq in xrange(len(cumulative_count)):
+
+                    data = pickle.load(open(cumulative_count[freq], "rb"))
+
+                    for group in data:
+                        histogram_file.write("%d,%d\n"%(group,freq+1))
+                    histogram_summary.write("%d,%d\n"%(freq+1,len(data)))
+                print("CPU PERCENT: %f"%curproc.cpu_percent())
+                print(psutil.virtual_memory())
+        pass
+    #cumulative_database.close()
+
 def calculate_all(num_resolvers=2):
     cumulative_count=[set()]
     curproc=psutil.Process()
@@ -93,7 +155,6 @@ def calculate_all(num_resolvers=2):
             for combination in combinations:
                 combination = tuple(sorted(combination))
                 tuple_hash = hash(combination)
-
                 for freq in xrange(len(cumulative_count)):
                     if tuple_hash in cumulative_count[freq]:
                         cumulative_count[freq].remove(tuple_hash)
@@ -135,14 +196,15 @@ def calculate_all_old(num_resolvers=2):
                 combination = tuple(sorted(combination))
                 tuple_hash = hash(combination)
                 tuple_str = ",".join(combination)
-                #cursor.execute('''INSERT OR IGNORE INTO resolution_combinations VALUES(?, ?)''', (tuple_hash,tuple_str))
-                cumulative_count.append(tuple_hash)
+                cursor.execute('''INSERT OR IGNORE INTO resolution_combinations VALUES(?, ?)''', (tuple_hash,tuple_str))
+                #cumulative_count.append(tuple_hash)
+            cumulative_database.commit()
             print("PROGRESS %d"%ind)
             ind+=1
             print("CPU PERCENT: %f"%curproc.cpu_percent())
             print(psutil.virtual_memory())
     except IOError:
-        cumulative_database.commit()
+        #cumulative_database.commit()
         distribution={}
         with open("%s/cumulative_%dr_data"%(DATA_DIRECTORY, num_resolvers),"w") as histogram_file:
             counted = Counter(cumulative_count)
