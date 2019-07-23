7f3074b0513cbdcb013f754cb0066547a8b9d15f
minivan <ivlin112@gmail.com>
2019-07-23 13:24:28 +0900

    integrated selenium in order to allow for testing with adblock on

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#
 import sys, os, re, sqlite3
 import subprocess
 import itertools
@@ -6,12 +10,55 @@ import time
 from collections import Counter
 
 def test_domain(domain, out_path, num_trials=1):
+    driver = webdriver.Firefox()
+
+    extension_dir = "~/.mozilla/firefox/au1p11i6.default-default/extensions/"
+    extensions = [
+        'uBlock0@raymondhill.net.xpi',
+    ]
+
+    for extension in extensions:
+        driver.install_addon(extension_dir + extension, temporary=True)
+
+    time.sleep(3)
+
+    outname="%s/tcpdump_raw"%out_path
+    with open(outname, "w") as tcpout:
+        #Begin TCPDump
+        #sudo_password="altairdenebvega"
+        #sudo_password="fe2d2cd42390e2b300b548eb80f3c23f36ea41"
+        command = 'sudo tcpdump'.split()
+        p = subprocess.Popen(['sudo', '-S'] + command, stdin=subprocess.PIPE, stderr=tcpout, stdout=tcpout,
+          universal_newlines=True)
+        time.sleep(3)
+        #automation issue: tcpdump must be run as sudo separately, but we must also pass the password to it
+        #for now, it will be done manually
+        #sudo_prompt = p.communicate(sudo_password + '\n')[1]
+
+        #Run Selenium
+        driver.get(domain)
+
+
+        time.sleep(2)
+        subprocess.check_call(["sudo","kill","-9",str(p.pid)])
+        os.waitpid(p.pid, 0)
+
+        #assert "Python" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        driver.close()
+    return outname
+
+def test_domain_browsertime(domain, out_path, num_trials=1):
     outname="%s/tcpdump_raw"%out_path
     # credit to jfs, https://stackoverflow.com/questions/13045593/using-sudo-with-python-script
     # for code on running sudo commands in python
     with open(outname, "w") as tcpout:
         #Begin TCPDump
-        sudo_password=fe2d2cd42390e2b300b548eb80f3c23f36ea41
+        #sudo_password="fe2d2cd42390e2b300b548eb80f3c23f36ea41"
         command = 'sudo tcpdump'.split()
         p = subprocess.Popen(['sudo', '-S'] + command, stdin=subprocess.PIPE, stderr=tcpout, stdout=tcpout,
           universal_newlines=True)
@@ -23,6 +70,8 @@ def test_domain(domain, out_path, num_trials=1):
         #Run Browsertime
         print("Testing domain %s"%domain)
         os.system("./browsertime.js -b firefox -n %d %s"%(num_trials, domain))
+
+
         subprocess.check_call(["sudo","kill","-9",str(p.pid)])
         os.waitpid(p.pid, 0)
     return outname
@@ -32,6 +81,8 @@ def parse_dns_records(filename,out_path):
         tcplist = tcpout.readlines()
         dns_req_captures=[]
         domain_captures=set()
+        print(len(tcplist))
+        ind=0
         for line in tcplist:
             # A? indicates IPv4, AAAA? indicates IPv6, this captures both, also give higher priority to ignoring www.
             capture = re.search("A\?\s(.+). ",line)
@@ -39,6 +90,9 @@ def parse_dns_records(filename,out_path):
                 dns_req_captures.append(line)
                 domain = capture.group(1)[4:] if capture.group(1).startswith("www.") else capture.group(1)
                 domain_captures.add(domain)
+                print [ind, line, domain_captures]
+            ind+=1
+        print ind
         with open("%s/dns_aux"%out_path,"w") as dns_aux:
             for line in dns_req_captures:
                 dns_aux.write(line)
@@ -82,7 +136,7 @@ def extract_combinations(filename, group_size):
 
 DATA_DIRECTORY="tcpout"
 
-def calculate_all(num_resolvers=2):
+def calculate_all_bad(num_resolvers=2):
     cumulative_count=[]
     cumulative_database = sqlite3.connect("db/combination_database_%dr"%num_resolvers)
     cursor = cumulative_database.cursor()
@@ -176,46 +230,6 @@ def calculate_all_old(num_resolvers=2):
         pass
     cumulative_database.close()
 
-
-def xyz(num_resolvers=2):
-    cumulative_count=[]
-    cumulative_database = sqlite3.connect("db/combination_database_%dr"%num_resolvers)
-    cursor = cumulative_database.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS resolution_combinations (tuple_hash VARCHAR(30), tuple_str VARCHAR(100))''')
-    curproc=psutil.Process()
-    try:
-        ind=0
-        while True:
-            path="%s/%d/dns_main"%(DATA_DIRECTORY, ind)
-            combinations = extract_combinations(path, num_resolvers)
-            for combination in combinations:
-                combination = tuple(sorted(combination))
-                tuple_hash = hash(combination)
-                tuple_str = ",".join(combination)
-                #cursor.execute('''INSERT OR IGNORE INTO resolution_combinations VALUES(?, ?)''', (tuple_hash,tuple_str))
-                cumulative_count.append(tuple_hash)
-            print("PROGRESS %d"%ind)
-            ind+=1
-            print("CPU PERCENT: %f"%curproc.cpu_percent())
-            print(psutil.virtual_memory())
-    except IOError:
-        with open("%s/cumulative_%dr"%(DATA_DIRECTORY, num_resolvers),"w") as histogram_file:
-            counted = Counter(cumulative_count)
-            len_counted=len(counted)
-            progress=0
-            ind=0.0
-            for combination in counted:
-                if (ind/len_counted * 20 > progress):
-                    print("%d percent of the way there\n"%(progress*5))
-                    progress+=1
-                ind+=1
-                cursor.execute('''SELECT * FROM resolution_combinations WHERE tuple_hash = ?''', (combination,))
-                histogram_file.write("%s : %s\n"%(cursor.fetchone()[1], str(counted[combination])))
-            print("CPU PERCENT: %f"%curproc.cpu_percent())
-            print(psutil.virtual_memory())
-        pass
-    cumulative_database.close()
-
 def pull_data(num_trials):
     ind=0
     with open("example_domains","r") as f:
@@ -224,7 +238,7 @@ def pull_data(num_trials):
             os.mkdir(DATA_DIRECTORY)
         except Exception:
             pass
-        for domain in domains:
+        for domain in ["google.com"]:
             out_path="%s/%d"%(DATA_DIRECTORY, ind)
             try:
                 os.mkdir(out_path)
